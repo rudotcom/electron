281e423b3e0da3b995b33ebe26108a35bfb75507
Alex Troshkov <rudotcom@gmai.com>
2021-03-29 16:52:15 +0300

    email design

--- store/views.py
+++ store/views.py
@@ -379,6 +379,7 @@ class MakeOrderView(CartMixin, View):
             # print(teleg)
             # send_telegram(teleg)
             html = render_to_string('order_placed.html', {'user': user, 'order': order})
+
             send_mail('Заказ в магазине Интроверт', 'Спасибо за Ваш заказ в магазине Интроверт!',
                       'Интроверт<noreply@introvert.com.ru>', [user.email], fail_silently=False, html_message=html)
 
