d15fd3a8ccd31d3f10fbc8edf75e05aa2a9731e1
Alex Troshkov <rudotcom@gmai.com>
2021-05-10 16:44:54 +0300

    fixed fake add-to-cart request

--- Introvert/urls.py
+++ Introvert/urls.py
@@ -5,6 +5,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 from django.views.generic.base import TemplateView
+from django.views.decorators.csrf import csrf_exempt
 
 from store.views import (
     BaseView,
@@ -26,7 +27,7 @@ from store.views import (
     GiftListView,
     ArticleView,
 
-    EmailView, WelcomeView, YooStatusView,
+    EmailView, WelcomeView, YooStatusView, TgView,
 )
 
 urlpatterns = [
@@ -53,6 +54,7 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(next_page="/"), name='logout'),
     path('registration/', RegistrationView.as_view(), name='registration'),
     path('profile/', ProfileView.as_view(), name='profile'),
+    path('tg/', csrf_exempt(TgView.as_view()), name='tg'),
 
     path(
         'admin/password_reset/',
